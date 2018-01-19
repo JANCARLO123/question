e00720cc5dc602d0c2c697d2ef6625deda76fd3a
Jan Carlo Perez <carlo.vallejo@citi.com.mc>
2018-01-19 15:18:20 -0600

    Correciones para base de datos

---
+++ src/main/java/com/jcpv/example/questionmarks/config/mvc/WebConfig.java
@@ -0,0 +1,32 @@
+package com.jcpv.example.questionmarks.config.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * Created by jancarlo on 18/01/18.
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+}
--- src/main/java/com/jcpv/example/questionmarks/controller/HomeController.java
+++ src/main/java/com/jcpv/example/questionmarks/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.jcpv.example.questionmarks.controller;
 
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by jancarlo on 10/01/18.
  */
 
-@Log
+//@Log
 @Controller
 public class HomeController {
 
-
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Value("${messages.home:default-value}")
     private String message = "Hello World";
 
     @RequestMapping("/")
     public String welcome(Model model) {
-       // log.info("Spring Boot Thymeleaf Configuration Example");
+         log.info("Spring Boot Thymeleaf Configuration Example");
         model.addAttribute("message", message);
         return "home";
     }
